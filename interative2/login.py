b83dbd45714b933602e1db67670a2cef47259a68
JiayiZhaoUofT <37254657+JiayiZhaoUofT@users.noreply.github.com>
2018-10-01 12:48:46 -0400

    construction of login

---
+++ interative2/login.py
@@ -0,0 +1,43 @@
+import flask as fk
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+from sqlalchemy.orm import sessionmaker
+import tabledef
+engine = tabledef.create_engine('sqlite:///tutorial.db', echo=True)
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss"
+
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    POST_USERNAME = str(request.form['username'])
+    POST_PASSWORD = str(request.form['password'])
+
+    SQLSession = sessionmaker(bind=engine)
+    s = tabledef.Session()
+    query = s.query(tabledef.User).filter(tabledef.User.username.in_([POST_USERNAME]), tabledef.User.password.in_([POST_PASSWORD]))
+    result = query.first()
+    if result:
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
+
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True, host='0.0.0.0', port=4000)
